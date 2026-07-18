77d03743d7bab1211c9cd23101613faae3e5dd11
easymomfoods <easymom404@gmail.com>
2026-07-18 19:11:04 +0530

    Add Content-Security-Policy header

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,21 @@ const nextConfig: NextConfig = {
         source: "/((?!api).*)",
         headers: [
           { key: "Access-Control-Allow-Origin", value: "https://easymom.co.in" },
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.clarity.ms https://www.google-analytics.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com data:",
+              "img-src 'self' https://res.cloudinary.com https://www.google-analytics.com https://www.googletagmanager.com data: blob:",
+              "connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://www.clarity.ms wss://www.clarity.ms",
+              "frame-src 'none'",
+              "object-src 'none'",
+              "base-uri 'self'",
+              "form-action 'self'",
+            ].join("; "),
+          },
         ],
       },
     ];
